39ad367393a784d92058d7c93dbaa5e1580acfd0
Monica <tutor.monica.carta.2012@gmail.com>
2023-10-31 13:02:29 +0000

    Added comments

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,10 @@
+//Prompt the user to enter a message and store the input in the userInput variable
 const userInput = prompt("Enter a message:");
+//Check if userInput is not empty
 if (userInput) {
+  //If the user entered a message, display an alert
   alert("You entered:" + userInput);
 } else {
+  //If userInput is empty display na different alert
   alert("No message provided");
 }
