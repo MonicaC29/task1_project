e05db6495e2feaf49c6f58155dba3201019cbf61
Monica <tutor.monica.carta.2012@gmail.com>
2023-10-31 13:16:45 +0000

    Changed code

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,11 @@
 //Prompt the user to enter a message and store the input in the userInput variable
 const userInput = prompt("Enter a message:");
+const messageContainer = document.getElementById("messageContainer");
 //Check if userInput is not empty
 if (userInput) {
   //If the user entered a message, display an alert
-  alert("You entered:" + userInput);
+  messageContainer.innerHTML = "You entered:" + userInput;
 } else {
   //If userInput is empty display na different alert
-  alert("No message provided");
+  messageContainer.innerHTML = "Invalid message";
 }
